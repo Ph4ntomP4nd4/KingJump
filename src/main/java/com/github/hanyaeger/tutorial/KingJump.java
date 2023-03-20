7710aa3810fc400f77b2dd979dfe8cc629b434a0
Ph4ntomP4nd4 <aniel1223@hotmail.nl>
2023-03-20 10:19:01 +0100

    Eerste commit aan de hand van de github_workshop

---
+++ src/main/java/com/github/hanyaeger/tutorial/KingJump.java
@@ -0,0 +1,15 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class KingJump extends YaegerGame {
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
