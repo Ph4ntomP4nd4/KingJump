259195202607138b0cdecf720faf6a6aa2ce00bb
Aniel Sedighi <a.sedighi1@student.han.nl>
2023-03-29 11:41:11 +0200

    player kan nu alleen een keer jumpen tot dat hij weer collide
    EnemyTileMap geupdate en layout gemaakt
    
    LevelScene layout verandert

--- src/main/java/KingJump/Character/Player.java
+++ src/main/java/KingJump/Character/Player.java
@@ -19,7 +19,9 @@ import java.util.Set;
 
 public class Player extends DynamicSpriteEntity implements KeyListener, SceneBorderTouchingWatcher, Newtonian, Collided {
     private HealthText healthText;
+    private boolean canJump = true;
     private int health = 3;
+
     public Player(Coordinate2D location, HealthText healthText) {
         super("sprites/playerSprites.png", location, new Size(80, 100), 1, 10);
         this.healthText = healthText;
@@ -28,23 +30,26 @@ public class Player extends DynamicSpriteEntity implements KeyListener, SceneBor
 
     @Override
     public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
-        if(pressedKeys.contains(KeyCode.A)){
-            setMotion(2,270d);
-            setGravityConstant(0.5);
+        if (pressedKeys.contains(KeyCode.A)) {
+            setMotion(2.5, 270d);
+            setGravityConstant(2.5);
             setCurrentFrameIndex(4);
             setCurrentFrameIndex(3);
-        } else if(pressedKeys.contains(KeyCode.D)){
-            setMotion(2,90d);
-            setGravityConstant(0.5);
+            canJump = false;
+        } else if (pressedKeys.contains(KeyCode.D)) {
+            setMotion(2.5, 90d);
+            setGravityConstant(2.5);
             setCurrentFrameIndex(5);
             setCurrentFrameIndex(6);
-        } else if(pressedKeys.contains(KeyCode.W)){
-            setMotion(5,180d);
-            setGravityConstant(0.05);
+            canJump = false;
+        } else if (canJump && pressedKeys.contains(KeyCode.W)) {
+            setMotion(20, 180d);
+            setGravityConstant(0.5);
             setCurrentFrameIndex(7);
             setCurrentFrameIndex(8);
-        } else if(pressedKeys.isEmpty()){
-            setGravityConstant(0.05);
+            canJump = false;
+        } else if (pressedKeys.isEmpty()) {
+            setGravityConstant(0.5);
         }
     }
 
@@ -67,10 +72,12 @@ public class Player extends DynamicSpriteEntity implements KeyListener, SceneBor
                 break;
         }
     }
+
     @Override
     public void onCollision(List<Collider> colliders) {
         //TODO Hitbox hitbox = new Hitbox((getWidth() / 2)(getHeight() - 200), 200, 80);
         setMotion(0, 0d);
+        canJump = true;
 //            health--;
 //            healthText.setHealthText(health);
 //            System.out.println(health);
--- src/main/java/KingJump/Scene/LevelScene.java
+++ src/main/java/KingJump/Scene/LevelScene.java
@@ -1,7 +1,5 @@
 package KingJump.Scene;
 
-import KingJump.Character.Enemy.Enemy;
-import KingJump.Character.Enemy.MeleeEnemy;
 import KingJump.Map.EnemyTileMap;
 import KingJump.Character.Player;
 import KingJump.InteractiveObject.FinishDoor;
@@ -11,32 +9,23 @@ import KingJump.InteractiveObject.Gem.GreenGem;
 import KingJump.InteractiveObject.Gem.RedGem;
 import KingJump.InteractiveObject.Star;
 import KingJump.Map.PlatformTileMap;
-import KingJump.Platform.NormalPlatform;
-import KingJump.Platform.Platform;
 import KingJump.Text.HealthText;
 import com.github.hanyaeger.api.Coordinate2D;
-import com.github.hanyaeger.api.Size;
-import com.github.hanyaeger.api.scenes.ScrollableDynamicScene;
+import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.api.scenes.TileMap;
 import com.github.hanyaeger.api.scenes.TileMapContainer;
 
-public class LevelScene extends ScrollableDynamicScene implements TileMapContainer {
+public class LevelScene extends DynamicScene implements TileMapContainer {
     @Override
     public void setupScene() {
         setBackgroundImage("backgrounds/levelbackground1.png");
         //TODO setBackgroundAudio("audio/waterworld.mp3"); (EEN LEUKE SOUNDTRACK)
-        setSize(new Size(1920, 1080));
-//        for (double i = 1; i <=1; i -= 0.01) {
-        setVerticalScrollPosition(1);
-//        }
     }
 
     @Override
     public void setupEntities() {
         Player player = new Player(new Coordinate2D(getWidth() / 2, getHeight() / 2), new HealthText(new Coordinate2D(50, 50)));
         addEntity(player);
-//        Platform platform = new NormalPlatform(new Coordinate2D(getWidth() / 2, getHeight() - 200), new Size(200, 80));
-//        addEntity(platform);
         Star star = new Star(new Coordinate2D(getWidth() / 2, getHeight() - 250));
         addEntity(star);
         FinishDoor finishDoor = new FinishDoor(new Coordinate2D(getWidth() / 2 + 50, getHeight() - 275));
@@ -47,8 +36,6 @@ public class LevelScene extends ScrollableDynamicScene implements TileMapContain
         addEntity(redGem);
         addEntity(blueGem);
         addEntity(greenGem);
-        Enemy meleeEnemy = new MeleeEnemy(new Coordinate2D(getWidth() / 3, getHeight() / 2));
-        addEntity(meleeEnemy);
 
 //        HealthText healthText = new HealthText(new Coordinate2D(50, 50));
 //        addEntity(healthText);
@@ -58,8 +45,8 @@ public class LevelScene extends ScrollableDynamicScene implements TileMapContain
     public void setupTileMaps() {
         PlatformTileMap platformTileMap = new PlatformTileMap();
         addTileMap(platformTileMap);
-//        EnemyTileMap enemyTilemap = new EnemyTileMap();
-//        addTileMap(enemyTilemap);
+        EnemyTileMap enemyTilemap = new EnemyTileMap();
+        addTileMap(enemyTilemap);
     }
 }
 
